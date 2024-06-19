4329d91a720dfac761deea4bcaba7e25a29c8cf3
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2024-06-19 12:30:28 +0530

    Updated app.js with improved calculations

--- app.js
+++ app.js
@@ -4,32 +4,56 @@ function getValues() {
   return [number1, number2];
 }
 
+function isEmptyInput(number1, number2) {
+  if (isNaN(number1) || isNaN(number2)) {
+    alert("Please enter valid numbers");
+    return true;
+  }
+  return false;
+}
+
 function add() {
   let [number1, number2] = getValues();
+  if (isEmptyInput(number1, number2)) return;
+
   let sum = number1 + number2;
   document.getElementById("result").textContent = sum;
 }
 
 function sub() {
   let [number1, number2] = getValues();
-  let difference = number1 - number2;
-  document.getElementById("result").textContent = difference;
+  if (isEmptyInput(number1, number2)) return;
+
+  let sub = number1 - number2;
+  document.getElementById("result").textContent = sub;
 }
 
 function mul() {
   let [number1, number2] = getValues();
-  let product = number1 * number2;
-  document.getElementById("result").textContent = product;
+  if (isEmptyInput(number1, number2)) return;
+
+  let mul = number1 * number2;
+  document.getElementById("result").textContent = mul;
 }
 
 function div() {
   let [number1, number2] = getValues();
-  if (number2 !== 0) {
-    let quotient = number1 / number2;
-    document.getElementById("result").textContent = quotient;
-  } else {
+  if (isEmptyInput(number1, number2)) return;
+
+  if (number2 === 0) {
     alert(
       "Division by zero is not allowed. Please enter a non-zero value for the second number."
     );
+    document.getElementById("num2").value = "";
+    return;
   }
+
+  let div = number1 / number2;
+  document.getElementById("result").textContent = div;
+}
+
+function clr() {
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
+  document.getElementById("result").textContent = "";
 }
